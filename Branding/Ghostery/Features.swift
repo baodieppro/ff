b566044ce84f1f16981b2f8462222fdffea67117
diepgiabao <64207337+diepgiabao@users.noreply.github.com>
2020-09-21 16:52:07 +0700

    Update Features.swift

--- Branding/Ghostery/Features.swift
+++ Branding/Ghostery/Features.swift
@@ -8,15 +8,15 @@
 
 import Shared
 
-extension Features.BrowserCore {
-    public static var configUrl: String {
-        return "https://api.ghostery.net/api/v1/config"
+extension Features.Search.AdditionalSearchEngines {
+    public static var isEnabled: Bool {
+        return false
     }
 }
 
-extension Features.Search.AdditionalSearchEngines {
+extension Features.Home.DynamicBackgrounds {
     public static var isEnabled: Bool {
-        return true
+        return false
     }
 }
 
@@ -35,43 +35,55 @@ extension Features.Search.QuickSearch {
     }
 }
 
+extension Features.PrivacyDashboard {
+    public static var isAntiTrackingEnabled: Bool {
+        return true
+    }
+    public static var isAdBlockingEnabled: Bool {
+        return true
+    }
+    public static var isPopupBlockerEnabled: Bool {
+        return true
+    }
+}
+
+extension Features.News {
+    public static var isEnabled: Bool {
+        return false
+    }
+}
+
+extension Features.Icons {
+    public static var type: IconType {
+        return .favicon
+    }
+}
+
+extension Features.AntiPhishing {
+    public static var isEnabled: Bool {
+        return false
+    }
+}
+
 extension Features.ControlCenter.PrivacyStats.SearchStats {
     public static var isEnabled: Bool {
         return false
     }
 }
 
-extension Features.Home.DynamicBackgrounds {
+extension Features.Telemetry {
+    public static var isEnabled: Bool {
+        return false
+    }
+}
+
+extension Features.TodayWidget {
     public static var isEnabled: Bool {
         return false
     }
 }
 
 extension Features.HumanWeb {
-    public static var isEnabled: Bool {
-        return true
-    }
-    public static var collectorDirectUrl: String {
-        return "https://collector-hpn.ghostery.net"
-    }
-    public static var collectorProxyUrl: String {
-        return "https://collector-hpn.ghostery.net"
-    }
-}
-
-extension Features.Telemetry {
-    public static var brand: String {
-        return "ghostery"
-    }
-    public static var anolysisUrl: String {
-        return "https://anolysis.privacy.ghostery.net"
-    }
-    public static var anolysisStagingUrl: String {
-        return "https://anolysis-staging.privacy.ghostery.net"
-    }
-}
-
-extension Features.PrivacyDashboard.ReportPage {
     public static var isEnabled: Bool {
         return false
     }
